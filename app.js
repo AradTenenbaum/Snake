8cf805a8e8c14572dc6a28716ec379bcd9cb31dc
Arad Tenenbaum <arad.tenen@gmail.com>
2022-06-23 16:15:12 +0300

    'Finished-Snake-Game'

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-import {SNAKE_SPEED, update as updateSnake, draw as drawSnake} from './snake.js';
-
+import {SNAKE_SPEED, update as updateSnake, draw as drawSnake, getSnakeHead, snakeIntersection} from './snake.js';
+import {update as updateFood, draw as drawFood} from './food.js';
+import { outsideGrid } from './grid.js';
 
 let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.getElementById('game-board');
 
 function main(currentTime) {
+
+  if(gameOver) {
+    if(confirm('You lost. Press ok to restart')) {
+      window.location = '/';
+    }
+    return;
+  }
+
   window.requestAnimationFrame(main);
   
   const secondSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if(secondSinceLastRender < (1 / SNAKE_SPEED)) return;
 
-  console.log('render')
 
   lastRenderTime = currentTime;
 
@@ -22,8 +32,16 @@ window.requestAnimationFrame(main);
 
 function update() {
   updateSnake();
+  updateFood();
+  checkDeath();
 }
 
 function draw () {
-  drawSnake();
+  gameBoard.innerHTML = '';
+  drawSnake(gameBoard);
+  drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
